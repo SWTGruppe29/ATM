00a00d354a47719713b1fae5a0e6fa6e74f37e14
Mathiasnielsen <au584107@uni.au.dk>
2019-03-06 18:25:20 +0100

    Class Templates

---
+++ ATM/Classes/ATMSystem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ATM.Interfaces;
+
+namespace ATM.Classes
+{
+    class ATMSystem : IATMSystem
+    {
+    }
+}
---
+++ ATM/Classes/Track.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ATM.Interfaces;
+
+namespace ATM.Classes
+{
+    public class Track : ITrack
+    {
+
+        public int Tag { get; set; }
+        public int Altitude { get; set; }
+        
+
+    }
+}
