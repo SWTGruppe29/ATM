9448ec5b7196304735fed44a2c3c06bb19fe4978
Mathias <math084m@hotmail.com>
2019-03-12 13:18:17 +0100

    ffff

--- ATM/Classes/Track.cs
+++ ATM/Classes/Track.cs
@@ -9,10 +9,11 @@ namespace ATM.Classes
 {
     public class Track : ITrack
     {
-
         public int Tag { get; set; }
         public int Altitude { get; set; }
-        
-
+        public int XCoordinate { get; set; }
+        public int YCoordinate { get; set; }
+        public int CurrentCompCourse { get; set; }
+        public int Velocity { get; set; }
     }
 }
