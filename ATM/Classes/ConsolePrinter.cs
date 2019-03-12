d09db6fe35af8737e1a686ea6bca07c104c57b3d
Mathiasnielsen <au584107@uni.au.dk>
2019-03-12 16:22:34 +0100

    Revert "ConsolePrinter and Logger completede and tested"
    This reverts commit 6526b57754a5ec65c8907c5942ac7938ae3ecc85.

--- ATM/Classes/ConsolePrinter.cs
+++ ATM/Classes/ConsolePrinter.cs
@@ -10,48 +10,32 @@ namespace ATM.Classes
     public class ConsolePrinter : IConsolePrinter
     {
         /// <summary>
-        /// Receives a List of tracks currently in the airspace, and tracks in conflict.
-        /// Tracks must have compass
+        /// Receives a List of tracks currently in the airspace, and tracks in conflict by 
         /// </summary>
         /// <param name="tracks">Tracks currently in the airspace</param>
         /// <param name="conflictTags">Tracks in breaking separation condition in the airspace</param>
         public static void Print(List<Track> tracks, string[] conflictTags)
         {
-            //Console.Clear();
-            
-
-            if (!tracks.Any())
-            {
-                Console.WriteLine("No airplanes currently in the airspace");
-                return;
-            }
-
-            Console.WriteLine("Airplane Tracks currently in the airspace:");
+            Console.Clear();
+            Console.WriteLine("Airplane Tracks currently in the airspace:\n");
 
             foreach (var track in tracks)
             {
-                if (track.Velocity != null && track.CurrentCompCourse != null) //Only display if it has velocity and compcourse
                 Console.WriteLine($"Flight tag: {track.Tag} " +
                                   $"X: {track.XCoordinate} " +
                                   $"Y: {track.YCoordinate} " +
                                   $"Altitude: {track.Altitude} " +
-                                  $"Horizontal Velocity: {track.Velocity} " +
+                                  $"Horizontal Velocity: {track.Velocity}" +
                                   $"Compass Course: {track.CurrentCompCourse}");
-            }   
-            Console.WriteLine();
-            
-            if (!conflictTags.Any())
-            {
-                Console.WriteLine("No Airplanes in conflict");
-                return;
             }
+            Console.WriteLine("\nAirplane tags in conflict");
 
-            Console.WriteLine("Airplanes in conflict");
             foreach (var tag in conflictTags)
             {
                 Console.WriteLine($"Airplane tag: {tag}");
             }
         }
+
 
         /// <summary>
         /// Receives a List of tracks currently in the airspace, and tracks in conflict by 
--- ATM/Classes/Track.cs
+++ ATM/Classes/Track.cs
@@ -13,30 +13,8 @@ namespace ATM.Classes
         public int Altitude { get; set; }
         public int XCoordinate { get; set; }
         public int YCoordinate { get; set; }
-        public double? CurrentCompCourse { get; set; }
-        public double? Velocity { get; set; }
+        public double CurrentCompCourse { get; set; }
+        public double Velocity { get; set; }
         public DateTime LastDateUpdate { get; set; }
-
-
-        public Track(string tag, int altitude, int x, int y, DateTime trackTime)
-        {
-            Tag = tag;
-            Altitude = altitude;
-            XCoordinate = x;
-            YCoordinate = y;
-            LastDateUpdate = trackTime;
-        }
-
-        public Track(string tag, int altitude, int x, int y, DateTime trackTime, double compass, double velocity)
-        {
-            Tag = tag;
-            Altitude = altitude;
-            XCoordinate = x;
-            YCoordinate = y;
-            LastDateUpdate = trackTime;
-            Velocity = velocity;
-            if (compass >= 0 && compass < 360)
-                CurrentCompCourse = compass;
-        }
     }
 }
