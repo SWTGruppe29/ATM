43a5c519a1f45cb843532511828429c0bbe45b21
Mathias <math084m@hotmail.com>
2019-03-12 16:24:35 +0100

    Angle lavet

--- ATM/Classes/TrackCalculator.cs
+++ ATM/Classes/TrackCalculator.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using ATM.Interfaces;
+using OfficeOpenXml.FormulaParsing.Excel.Functions.Math;
 
 namespace ATM.Classes
 {
@@ -11,23 +13,27 @@ namespace ATM.Classes
     {
         public double CalculateHorizontalVelocity(double Last_x, double Last_y, double New_x, double New_y)
         {
-            
-            double x = (New_x - Last_x) * (Math.PI / 180.0f);
-            double y = (New_y - Last_y) * (Math.PI / 180.0f);
-            double a = Math.Pow(Math.Sin(y / 2.0), 2)
-                         + Math.Cos(Last_y * (Math.PI / 180.0f))
-                         * Math.Cos(New_y * (Math.PI / 180f))
-                         * Math.Pow(Math.Sin(x / 2.0), 2);
-            double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
-            return 3956 * c;
+            return Last_y;
         }
 
         public double CalculateCompassCourse(double Last_x, double Last_y, double New_x, double New_y)
         {
-            double xDiff = New_x - Last_x;
-            double yDiff = New_y - Last_y;
+            double deltaY = New_y - Last_y;
+            double deltaX = New_x - New_y;
 
-            return Math.Atan2(yDiff, xDiff) * 180.0 / Math.PI;
+            double angle = Math.Atan2(deltaY, deltaX) * 180 / Math.PI;
+
+            if (angle < 0)
+            {
+                angle += 2*Math.PI;
+            }
+            
+            return (360-(Trim(angle)-90));
+        }
+
+        public double Trim(double deg)
+        {
+            return (Math.Round(deg, 5));
         }
     }
 }
--- ATMUnitTest/Class1.cs
+++ ATMUnitTest/Class1.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ATM;
+using ATM.Classes;
+using ATM.Interfaces;
+using NUnit.Framework;
 
 namespace ATMUnitTest
 {
+    [TestFixture]
     public class Class1
     {
+        private TrackCalculator _uut;
+        
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new TrackCalculator();
+        }
+
+        
+        [TestCase(58416, 94744, 58409, 94876)]
+        [TestCase(58423, 94612, 58416, 94744,22)]
+        public void TestCalcDegress(double Last_x, double Last_y, double New_x, double New_y, double result)
+        {
+            
+            Assert.That(_uut.CalculateCompassCourse(Last_x, Last_y, New_x, New_y),Is.EqualTo(result));
+        }
     }
 }
