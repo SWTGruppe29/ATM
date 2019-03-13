138d2a1be1226f470c8e042d6cd587b544dcc3f6
Mathiasnielsen <au584107@uni.au.dk>
2019-03-13 11:33:49 +0100

    CoverageTest

--- ATMUnitTest/ConsolePrinterTest.cs
+++ ATMUnitTest/ConsolePrinterTest.cs
@@ -13,7 +13,7 @@ namespace ATMUnitTest
     public class ConsolePrinterTest
     {
         [Test]
-        public void ConsolePrintTest()
+        public void TwoTracksInConflictAndAirspace()
         {
             DateTime time1 = new DateTime(2018, 03, 12, 14, 50, 25, 543);
             DateTime time2 = new DateTime(2016, 03, 12, 15, 50, 25, 543);
@@ -32,5 +32,29 @@ namespace ATMUnitTest
 
             ConsolePrinter.Print(uutTracks, Tags);
         }
+
+        [Test]
+        public void NoTracksInList()
+        {
+            List<Track> uutTracks = new List<Track>();
+            ConsolePrinter.Print(uutTracks, null);
+        }
+
+
+        [Test]
+        public void TwoTracksInAirspace()
+        {
+            DateTime time1 = new DateTime(2018, 03, 12, 14, 50, 25, 543);
+            DateTime time2 = new DateTime(2016, 03, 12, 15, 50, 25, 543);
+
+            List<Track> uutTracks = new List<Track>()
+            {
+                new Track("5412BJ", 12345, 15312,84393, time1, 154.5433, 1000),
+                new Track("5417KJ", 15445, 15342,84393, time1, 154.233, 987.123)
+            };
+
+            ConsolePrinter.Print(uutTracks, null);
+        }
+
     }
 }
